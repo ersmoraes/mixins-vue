82172a7b7bc5681845fc17636ca0f0176c9fbe6b
Eric Moraes <ersmoraes@outlook.com>
2022-04-18 15:13:52 -0300

    Mixins em VueJs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="display: flex; flex-direction: row;">
+
+    <div style="flex-basis: 33%">
+      <lista-filmes />
+    </div>
+
+    <div style="flex-basis: 33%">
+      <lista-pessoas />
+    </div>
+
+    <div style="flex-basis: 33%">
+      <lista-videogames />
+    </div> 
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListaFilmes from '@/components/ListaFilmes.vue'
+import ListaPessoas from '@/components/ListaPessoas.vue'
+import ListaVideogames from '@/components/ListaVideogames.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListaFilmes,
+    ListaPessoas,
+    ListaVideogames
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
